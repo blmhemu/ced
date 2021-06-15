415217aa301a9b4c6e616a32739748713722e434
Hemanth Bollamreddi <blmhemu@gmail.com>
2021-06-15 11:54:53 +0530

    Inital commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/blmhemu/consul-ext-dns
+
+go 1.16
+
+require github.com/hashicorp/consul/api v1.8.1
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/consul/api"
+)
+
+func main() {
+
+	config := api.Config{
+		Address: "127.0.0.1:8500",
+		Scheme:  "http",
+	}
+
+	// We recieve updates on this channel
+	updates := make(chan []*api.ServiceEntry)
+
+	// Watch for any changes in service
+	go watchLB(&config, updates)
+
+	// Process any changes
+	oldIPSet := make(map[string]struct{})
+	for svcs := range updates {
+		newIPSet := make(map[string]struct{})
+		for _, svc := range svcs {
+			newIPSet[svc.Node.Address] = struct{}{}
+		}
+		if changed(newIPSet, oldIPSet) {
+			newIPs := make([]string, 0, len(newIPSet))
+			for ip := range newIPSet {
+				newIPs = append(newIPs, ip)
+			}
+			// No IPs means lb is down
+			// either momentarily or for an extended period.
+			// We refrain from setting empty A records in DNS
+			// When lb is back online, we update the IPs anyways if there is a change
+			if len(newIPs) != 0 {
+				updateDNSRecords(newIPs)
+				oldIPSet = newIPSet
+			} else {
+				fmt.Println("Lb is down")
+			}
+		}
+	}
+}
+
+func changed(newIPSet, oldIPSet map[string]struct{}) bool {
+	if len(newIPSet) != len(oldIPSet) {
+		return true
+	}
+	for ip := range newIPSet {
+		if _, ok := oldIPSet[ip]; !ok {
+			return true
+		}
+	}
+	return false
+}
+
+func updateDNSRecords(ips []string) {
+	fmt.Println(ips)
+}
+
+type MyQueryOptions struct{ api.QueryOptions }
+
+func (qo *MyQueryOptions) fetchUpdates(client *api.Client) ([]*api.ServiceEntry, error) {
+	svccfg, qm, err := client.Health().Service("fabio", "", true, &qo.QueryOptions)
+
+	if err != nil || qm.LastIndex <= qo.WaitIndex {
+		qo.WaitIndex = 0
+	} else {
+		qo.WaitIndex = qm.LastIndex
+	}
+	return svccfg, err
+}
+
+func watchLB(config *api.Config, updates chan []*api.ServiceEntry) {
+	client, err := api.NewClient(config)
+	if err != nil {
+		panic(err)
+	}
+
+	qo := MyQueryOptions{
+		QueryOptions: api.QueryOptions{
+			RequireConsistent: true,
+			WaitIndex:         0,
+		},
+	}
+
+	for {
+		svccfg, err := qo.fetchUpdates(client)
+		if err != nil {
+			panic(err)
+		}
+		updates <- svccfg
+	}
+}
