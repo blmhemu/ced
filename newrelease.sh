d593f9c0dc4da7c79cbb5b175139315197a724d2
Hemanth Bollamreddi <blmhemu@gmail.com>
2021-06-17 14:47:14 +0530

    Fixed docker release

--- newrelease.sh
+++ newrelease.sh
@@ -1,4 +1,4 @@
-export CED_VERSION="0.1.3"
+export CED_VERSION="0.1.4"
 # Create git tags
 if git rev-parse "v$CED_VERSION" >/dev/null 2>&1; then
     echo "Tag already present"
@@ -10,4 +10,4 @@ fi
 # Run go releaser for binary releases
 goreleaser release --rm-dist &&
 # Run docker build push
-docker buildx build --platform linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64 --build-arg CED_VERSION=$CED_VERSION --push -t blmhemu/ced:$CED_VERSION -t blmhemu/fabio:latest .
+docker buildx build --platform linux/arm64,linux/arm/v7,linux/arm/v6,linux/amd64 --build-arg CED_VERSION=$CED_VERSION --push -t blmhemu/ced:$CED_VERSION -t blmhemu/ced:latest .
